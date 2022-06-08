73094140ad68d219e29a7718140364321446a790
Sachin Ramanathan <61686882+Sachin-dot-py@users.noreply.github.com>
2022-06-08 17:51:47 +0800

    Fixed spelling mistake

--- app.py
+++ app.py
@@ -173,13 +173,13 @@ async def on_thread_join(thread):
 
 @bot.event
 async def on_guild_join(guild):
-    global gpdp
-    gpdp.set_pref('rep_enabled', True, guild.id)
+    global gpdb
+    gpdb.set_pref('rep_enabled', True, guild.id)
     await guild.create_role(name="Reputed", color=0x3498db)  # Create Reputed Role
     await guild.create_role(name="100+ Rep Club", color=0xf1c40f)  # Create 100+ Rep Club Role
     await guild.create_role(name="500+ Rep Club", color=0x2ecc71)  # Create 500+ Rep Club Role
     await guild.system_channel.send(
-        "Hi! Please set server preferences using the slash command /set_preferences for this bot to function properly.")
+        "Hi! Please set all the server preferences using the slash command /set_preferences for this bot to function properly.")
 
 
 @bot.event
