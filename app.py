8f45a33a980bd69f9b9f69a9aebaa5286f46d907
Programmer1325 <krishith.vadivel@gmail.com>
2023-05-16 19:47:41 +0530

    Revamped keywords (uses modals and slash_commands)

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ from data import reactionroles_data, helper_roles, subreddits, study_roles
 
 TOKEN = os.environ.get("IGCSEBOT_TOKEN")
 LINK = os.environ.get("MONGO_LINK")
-GUILD_ID = 576460042774118420
+GUILD_ID = 882476325510721556
 
 intents = discord.Intents().all()
 bot = commands.Bot(command_prefix=".", intents=intents)
@@ -33,8 +33,8 @@ async def on_ready():
     for guild in bot.guilds:
         value = f"Owner: {guild.owner}\nMembers: {guild.member_count}\nBoosts: {guild.premium_subscription_count}"
         embed.add_field(name=guild.name, value=value, inline=True)
-    igcse = await bot.fetch_guild(576460042774118420)
-    logs = await igcse.fetch_channel(1017792876584906782)
+    igcse = await bot.fetch_guild(882476325510721556)
+    logs = await igcse.fetch_channel(1105813453916086302)
     await logs.send(embed=embed)
 
 
@@ -1135,6 +1135,9 @@ class KeywordsDB:
     def get_keywords(self, guild_id: int):
         result = self.keywords.find({"guild_id": guild_id}, {"_id": 0, "guild_id": 0})
         return {i['keyword'].lower(): i['autoreply'] for i in result}
+    
+    def keyword_list(self, guild_id: int):
+        return self.keywords.find({"guild_id": guild_id}, {"_id": 0, "guild_id": 0})
 
     def add_keyword(self, keyword: str, autoreply: str, guild_id: int):
         result = self.keywords.insert_one({"keyword": keyword.lower(), "autoreply": autoreply, "guild_id": guild_id})
@@ -1144,42 +1147,128 @@ class KeywordsDB:
         result = self.keywords.delete_one({"keyword": keyword.lower(), "guild_id": guild_id})
         return result
 
-
 kwdb = KeywordsDB(LINK)
 
+class AddKeywords(discord.ui.Modal):
+    def __init__(self):
+        super().__init__("Add a keyword", timeout=None)
 
-@bot.command(name="addkeyword", description="Add keywords (for mods)")
-async def addkeyword(ctx, keyword: str, autoresponse: str):
-    """ Put keyword and autoresponse in quotation marks and put a space between them"""
-    if not await isModerator(ctx.author):
-        await ctx.reply("You do not have the permissions to perform this action.")
-        return
-    kwdb.add_keyword(keyword, autoresponse, ctx.guild.id)
-    global keywords
-    keywords[ctx.guild.id] = kwdb.get_keywords(ctx.guild.id)
-    await ctx.reply(f"Created keyword {keyword} for autoresponse {autoresponse}")
+        self.keyword = discord.ui.TextInput(label="Add keyword", style=discord.TextInputStyle.short, placeholder="The keyword you would like to add", required=True)
+        self.autoresponse = discord.ui.TextInput(label="Add response", style=discord.TextInputStyle.paragraph, placeholder="The response you would like to be sent", required=True)
+
+        self.add_item(self.keyword)
+        self.add_item(self.autoresponse)
+    
+    async def callback(self, interaction: discord.Interaction):
+        kwdb.add_keyword(self.keyword.value, self.autoresponse.value, interaction.guild.id)
+        global keywords
+        keywords[interaction.guild.id] = kwdb.get_keywords(interaction.guild.id)
+        await interaction.send(f"Created keyword `{self.keyword.value}` for autoresponse `{self.autoresponse.value}`", ephemeral=True, delete_after=2)
+
+@bot.slash_command(description="Add keywords (for mods)")
+async def add_keyword(interaction: discord.Interaction):
+    if await isModerator(interaction.user):
+        await interaction.response.send_modal(modal=AddKeywords())
+
+@bot.slash_command(description="Delete keywords (for mods)")
+async def delete_keyword(interaction: discord.Interaction, keyword: str = discord.SlashOption(name="keyword", description="Keyword to delete", required=True)):
+    if await isModerator(interaction.user):
+        kwdb.remove_keyword(keyword, interaction.guild.id)
+        global keywords
+        keywords[interaction.guild.id] = kwdb.get_keywords(interaction.guild.id)
+        await interaction.send(f"Deleted keyword `{keyword}`", ephemeral=True, delete_after=2)
+
+@bot.slash_command(description="Display all keywords")
+async def list_keywords(interaction: discord.Interaction):
+    page = 1
+    
+    await interaction.response.defer()
+    keywords = kwdb.keyword_list(interaction.guild.id)  # List of keywords
+    keywords = [item.values() for item in keywords]  # Changing format of keywords
+    chunks = [list(keywords)[x:x + 9] for x in range(0, len(keywords), 9)]  # Split into groups of 9
+
+    pages = []
+    for n, chunk in enumerate(chunks):
+        embed = discord.Embed(title="List of keywords", description=f"Page {n + 1} of {len(chunks)}",
+                                 colour=discord.Colour.green())
+        for keyword, autoresponse in chunk:
+            embed.add_field(name=keyword, value="" + "\n", inline=True)
+        pages.append(embed)
+
+    if not page: page = 1
+
+    first, prev = discord.ui.Button(emoji="⏪", style=discord.ButtonStyle.blurple), discord.ui.Button(emoji="⬅️", style=discord.ButtonStyle.blurple)
+
+    nex, last = discord.ui.Button(emoji="➡️", style=discord.ButtonStyle.blurple), discord.ui.Button(emoji="⏩", style=discord.ButtonStyle.blurple)
+    if page == 1:
+        first.disabled, prev.disabled = True, True
+    view = discord.ui.View(timeout=120)
 
 
-@bot.command(name="deletekeyword", description="Delete keywords (for mods)")
-async def deletekeyword(ctx, keyword: str):
-    """ Put keyword in quotation marks"""
-    if not await isModerator(ctx.author):
-        await ctx.reply("You do not have the permissions to perform this action.")
-        return
-    kwdb.remove_keyword(keyword, ctx.guild.id)
-    global keywords
-    keywords[ctx.guild.id] = kwdb.get_keywords(ctx.guild.id)
-    await ctx.reply(f"Deleted keyword {keyword}")
+    async def timeout():
+        nonlocal message
+        disabled = discord.ui.View()
+        for b in view.children:
+            d = b
+            d.disabled = True
+            disabled.add_item(d)
+        await message.edit(view=disabled)
+    view.on_timeout = timeout
 
+    async def f_callback(b_interaction):
+        if b_interaction.user != interaction.user:
+            await b_interaction.response.send_message("This is not for you.", ephemeral=True)
+            return
+        nonlocal page
+        view = discord.ui.View(timeout=None)
+        first.disabled, prev.disabled, nex.disabled, last.disabled = True, True, False, False
+        view.add_item(first); view.add_item(prev); view.add_item(nex); view.add_item(last)
+        page = 1
+        await b_interaction.response.edit_message(embed=pages[page - 1], view=view)
 
-@bot.command(name="listkeywords", description="List keywords (for mods)")
-async def listkeywords(ctx):
-    """ Put keyword in quotation marks"""
-    if not await isModerator(ctx.author):
-        await ctx.reply("You do not have the permissions to perform this action.")
-        return
-    await ctx.reply(f"Active keywords: {list(kwdb.get_keywords(ctx.guild.id).keys())}")
+    async def p_callback(b_interaction):
+        if b_interaction.user != interaction.user:
+            await b_interaction.response.send_message("This is not for you.", ephemeral=True)
+            return
+        nonlocal page
+        page -= 1
+        view = discord.ui.View(timeout=None)
+        if page == 1:
+            first.disabled, prev.disabled, nex.disabled, last.disabled = True, True, False, False
+        else:
+            first.disabled, prev.disabled, nex.disabled, last.disabled = False, False, False, False
+        view.add_item(first); view.add_item(prev); view.add_item(nex); view.add_item(last)
+        await b_interaction.response.edit_message(embed=pages[page - 1], view=view)
 
+    async def n_callback(b_interaction):
+        if b_interaction.user != interaction.user:
+            await b_interaction.response.send_message("This is not for you.", ephemeral=True)
+            return
+        nonlocal page
+        page += 1
+        view = discord.ui.View(timeout=None)
+        if page == len(pages):
+            first.disabled, prev.disabled, nex.disabled, last.disabled = False, False, True, True
+        else:
+            first.disabled, prev.disabled, nex.disabled, last.disabled = False, False, False, False
+        view.add_item(first); view.add_item(prev); view.add_item(nex); view.add_item(last)
+        await b_interaction.response.edit_message(embed=pages[page - 1], view=view)
+
+    async def l_callback(b_interaction):
+        if b_interaction.user != interaction.user:
+            await b_interaction.response.send_message("This is not for you.", ephemeral=True)
+            return
+        nonlocal page
+        view = discord.ui.View(timeout=None)
+        first.disabled, prev.disabled, nex.disabled, last.disabled = False, False, True, True
+        view.add_item(first); view.add_item(prev); view.add_item(nex); view.add_item(last)
+        page = len(pages)
+        await b_interaction.response.edit_message(embed=pages[page - 1], view=view)
+
+    first.callback, prev.callback, nex.callback, last.callback = f_callback, p_callback, n_callback, l_callback
+    view.add_item(first); view.add_item(prev); view.add_item(nex); view.add_item(last)
+
+    message = await interaction.send(embed=pages[page - 1], view=view)
 
 # Misc Functions
 
