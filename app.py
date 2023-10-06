9f1712e07dcab88fa90f002058543762e7c607c6
Rahul <rahulmaru3507@gmail.com>
2023-10-06 21:44:38 +0530

    channellock (#1)
    * channellock
    
    * incorporate channellock into app.py
    
    * change everyone role and update task loop to run eevery 60s
    
    * remove TODOs
    
    * fix everyone role (remove hardcoded id)
    
    * delete r-igcse beta.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import traceback
 import ast
 import json
 
-from nextcord.ext import commands
+from nextcord.ext import tasks, commands
 from data import reactionroles_data, helper_roles, subreddits, study_roles
 
 # Set up a Discord API Token and a MongoDB Access Link in a .env file and use the command "heroku local" to run the bot locally.
@@ -28,7 +28,7 @@ igcse, logs = None, None
 async def on_ready():
     global igcse, logs
     print(f"Logged in as {str(bot.user)}.")
-    await bot.change_presence(activity=discord.Game(name="Flynn#5627"))
+    await bot.change_presence(activity=discord.Game(name="flynn5627"))
     embed = discord.Embed(title=f"Guilds Info ({len(bot.guilds)})", colour=0x3498db, description="Statistics about the servers this bot is in.")
     for guild in bot.guilds:
         value = f"Owner: {guild.owner}\nMembers: {guild.member_count}\nBoosts: {guild.premium_subscription_count}"
@@ -36,6 +36,7 @@ async def on_ready():
     igcse = await bot.fetch_guild(576460042774118420)
     logs = await igcse.fetch_channel(1017792876584906782)
     await logs.send(embed=embed)
+    checklocks.start()
 
 
 @bot.event
@@ -2304,11 +2305,112 @@ class Code(discord.ui.Modal):
         else:
             await interaction.send("There was an error compiling the code", ephemeral = True)
     
-
-
 @bot.slash_command(name = "code", description = "Compile a pseudocode code and send it back")
 async def code(interaction: discord.Interaction):
     await interaction.response.send_modal(modal = Code())
+
+
+async def togglechannellock(channelid, unlock, *, unlocktime=0):
+    """Function for locking/unlocking a discord channel"""
+
+    guild = bot.get_guild(GUILD_ID)
+    everyonerole = guild.roles.everyone
+
+    channel = bot.get_channel(channelid)
+    overwrite = channel.overwrites_for(everyonerole)
+    overwrite.send_messages = unlock
+
+    await channel.set_permissions(everyonerole, overwrite=overwrite)
+    await channel.send(f"{'Unl' if unlock else 'L'}ocked channel.")
+
+    if not unlock:
+      # If the channel was locked, send another embed with unlock time
+      embed = discord.Embed(description=f"Unlocking channel <t:{unlocktime}:R>.")
+      await channel.send(embed=embed)
+
+@bot.slash_command(name="channellock", description="Locks a channel at a specified time")
+async def lockcommand(interaction: discord.Interaction,
+                        channelinput: discord.TextChannel =  discord.SlashOption(name="channel_name", description="Which channel do you want to lock?", required=True),
+                        locktime: str = discord.SlashOption(name="lock_time", description="At what time do you want the channel to be locked?", required=True),
+                        unlocktime: str = discord.SlashOption(name="unlock_time", description="At what time do you want the channel to be unlocked?", required=True)):
+
+  # perms validation
+  if not await isModerator(interaction.user) or await hasRole(interaction.user, "Bot Developer"):
+        await interaction.send(f"Sorry {interaction.user.mention},"
+                               "you don't have the permission to perform this action.",
+                                ephemeral=True)
+        return
+
+  # input validation
+  try:
+    locktime = int(locktime)
+    unlocktime = int(unlocktime)
+  except ValueError:
+    await interaction.send("Times must be (positive) integers.", ephemeral=True)
+    return
+
+  # + 1 is for cancelling the truncation
+  t = int(time.time()) + 1
+  if locktime < 0 or unlocktime < 0:
+    await interaction.send("Lock time must be positive.", ephemeral=True)
+    return
+  elif locktime >= unlocktime :
+    await interaction.send("Unlock time must be after lock time.", ephemeral=True)
+    return
+  elif unlocktime < t:
+    await interaction.send(f"Unlock time has already passed (current time: {round(time.time())}).", ephemeral=True)
+    return
+
+  locktimeinunix = f"<t:{locktime}:F>"
+  unlocktimeinunix = f"<t:{unlocktime}:F>"
+  await interaction.send(f"<#{channelinput.id}> is scheduled to lock on "
+                         f"{locktimeinunix} and unlock on {unlocktimeinunix}",
+                          ephemeral=True)
+
+  channelid = f"<#{channelinput.id}>"
+  logchannel = bot.get_channel(947859228649992213)
+  await logchannel.send(f'Channel Name: {channelid}\n'
+                     f'Lock Time: {locktime} ({locktimeinunix})\n'
+                     f'Unlock Time: {unlocktime} ({unlocktimeinunix})\n')
+
+  client = pymongo.MongoClient(LINK)
+  db = client.IGCSEBot
+  locks = db["channellock"]
+
+  # inserts the lock and unlock as 2 separate entries
+  locks.insert_one({"_id": 'l' + str(t), "channelid": channelinput.id,
+                     "unlock": False, "time": locktime,
+                     "resolved": False})
+
+  locks.insert_one({"_id": 'u' + str(t), "channelid": channelinput.id,
+                     "unlock": True, "time": unlocktime,
+                     "resolved": False})
+
+  embed = discord.Embed(description=f"Locking channel <t:{max(locktime, t)}:R>.")
+  await channelinput.send(embed=embed)
+
+@tasks.loop(seconds=60)
+async def checklocks():
+  """Checks the database every 60 seconds to see if anything needs to be locked or unlocked """
+
+  client = pymongo.MongoClient(LINK)
+  db = client.IGCSEBot
+  locks = db["channellock"]
+
+  try:
+    results = locks.find({"resolved": False})
+
+    for result in results:
+      if result["time"] <= time.time():
+        # finds the unlock time
+        ult = locks.find_one({"_id": "u" + result["_id"][1:]})["time"]
+        await togglechannellock(result["channelid"], result["unlock"], unlocktime=ult)
+
+        # Resolves the database entry (to avoid repeated locking/unlocking)
+        locks.update_one({"_id": result["_id"]}, {"$set": {"resolved": True}})
+
+  except Exception as e:
+     print(e)
 
 
 bot.run(TOKEN)
