cc813509bac0747e9279de7e6168d2683a90e1fd
Juzcallmekaushik <76829835+juzcallmekaushik@users.noreply.github.com>
2023-12-28 22:34:20 +0800

    Update on_ready.py (#51)
    added `checktime.start()`

--- src/on_ready.py
+++ src/on_ready.py
@@ -1,6 +1,6 @@
 from constants import GUILD_ID, LOG_CHANNEL_ID
 from bot import discord, bot
-from checklocks import checklocks
+from checklocks import checklocks, checktime
 
 @bot.event
 async def on_ready():
@@ -8,7 +8,7 @@ async def on_ready():
     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="r/IGCSE"))
     embed = discord.Embed(title=f"Guilds Info ({len(bot.guilds)})", colour=0x3498db, description="Statistics about the servers this bot is in.")
     checklocks.start()
-
+    checktime.start()
     for guild in bot.guilds:
         value = f"Owner: {guild.owner}\nMembers: {guild.member_count}\nBoosts: {guild.premium_subscription_count}"
         embed.add_field(name=guild.name, value=value, inline=True)
