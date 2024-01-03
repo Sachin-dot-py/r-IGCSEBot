601bd6d60fee960904deae8a5cfd44d37c07bfbb
Juzcallmekaushik <76829835+juzcallmekaushik@users.noreply.github.com>
2024-01-03 14:55:16 +0800

    Update gostudy.py

--- src/gostudy.py
+++ src/gostudy.py
@@ -16,8 +16,8 @@ async def gostudy(interaction: discord.Interaction,
                  unmute_tim = int(((time.time()) + 1) + 3600)
                  await message.delete()
                  dm = await interaction.user.create_dm()
-                 await dm.send(f"Study time! You've been given a temporary break from the off-topic channels for the next hour, thanks to <@{interaction.user.id}>. Use this time to focus on your studies and make the most of it!\n\n Role will be removed <t:{unmute_tim}:R>")
-                 await interaction.user.add_roles(forced_mute_role)
+                 embed = discord.Embed(description = f"Study time! You've been given a temporary break from the off-topic channels for the next hour, thanks to <@{interaction.user.id}>. Use this time to focus on your studies and make the most of it!\n\nThe role will be removed <t:{unmute_tim}:R>")
+                 await dm.send(embed=embed)
                  timern = int(time.time()) + 1
                  unmute_time = int(((time.time()) + 1) + 3600)
                  client = pymongo.MongoClient(LINK)
