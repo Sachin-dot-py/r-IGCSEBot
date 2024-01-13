a69fbad8ede68626d698fac3517830a6be912c45
Juzcallmekaushik <76829835+juzcallmekaushik@users.noreply.github.com>
2024-01-13 11:52:09 +0800

    Delete src directory

--- src/bans.py
+++
@@ -1,8 +0,0 @@
-from bot import discord
-
-async def is_banned(user, guild):
-    try:
-        await guild.fetch_ban(user)
-        return True
-    except discord.NotFound:
-        return False
--- src/on_voice_state_update.py
+++
@@ -1,9 +0,0 @@
-from constants import GUILD_ID
-from bot import bot
-
-@bot.event
-async def on_voice_state_update(member, before, after):
-    if member.guild.id == GUILD_ID:
-        if before.channel:  # When user leaves a voice channel
-            if "study session" in before.channel.name.lower() and before.channel.members == []:  # If the study session is over
-                await before.channel.edit(name="General")  # Reset channel name
