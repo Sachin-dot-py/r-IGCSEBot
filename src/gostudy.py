10ee33fbac3fad653c880787af700703b53f8163
Juzcallmekaushik <76829835+juzcallmekaushik@users.noreply.github.com>
2024-01-14 18:54:59 +0800

    Update gostudy.py
    partially fixed gostudy. haven't tested yet

--- src/gostudy.py
+++ src/gostudy.py
@@ -6,29 +6,31 @@ from roles import has_role, get_role, is_helper, is_moderator, is_server_booster
 @bot.slash_command(name="gostudy", description="disables the access to the offtopics for 1 hour.")
 async def gostudy(interaction: discord.Interaction,
                   user: discord.User = discord.SlashOption(name="name", description="who do you want to use this command on? (for mods)", required=False)):
-        
+      
+      client = pymongo.MongoClient(LINK)
+      db = client.IGCSEBot
+      mute = db["mute"]
+      
       forced_mute_role = bot.get_guild(GUILD_ID).get_role(FORCED_MUTE_ROLE)
       if user == None:
             user_id = interaction.user.id
-            user = bot.get_guild(GUILD_ID).get_member(user_id)        
+            user = bot.get_guild(GUILD_ID).get_member(user_id)    
+            dm = await user.create_dm()    
             view = discord.ui.View(timeout=None)
             proceedBTN = discord.ui.Button(label="Proceed", style=discord.ButtonStyle.blurple)
             cancelBTN = discord.ui.Button(label="Cancel", style=discord.ButtonStyle.red)
+            
             async def proceedCallBack(interaction):
                  unmute_tim = int(((time.time()) + 1) + 3600)
                  await message.delete()
-                 dm = await user.create_dm()
                  embed = discord.Embed(description = f"Study time! You've been given a temporary break from the off-topic channels for the next hour, thanks to <@{interaction.user.id}>. Use this time to focus on your studies and make the most of it!\n\nThe role will be removed at <t:{unmute_tim}:f>, which is <t:{unmute_tim}:R>", color=0xAFE1AF)
                  await dm.send(embed=embed)
                  await user.add_roles(forced_mute_role)
                  timern = int(time.time()) + 1
                  unmute_time = int(((time.time()) + 1) + 3600)
-                 client = pymongo.MongoClient(LINK)
-                 db = client.IGCSEBot
-                 mute = db["mute"]
-                 mute.insert_one({"_id": str(timern), "user_id": str(user_id),
-                                  "unmute_time": str(unmute_time), "muted": True})
+                 mute.insert_one({"_id": str(timern), "user_id": str(user_id), "unmute_time": str(unmute_time), "muted": True})
             proceedBTN.callback = proceedCallBack
+            
             async def cancelCallBack(interaction):
                  await message.delete()
             cancelBTN.callback = cancelCallBack
@@ -37,29 +39,27 @@ async def gostudy(interaction: discord.Interaction,
             message = await interaction.send("Are we ready to move forward?", view=view, ephemeral=True)
       else:
             unmute_tim = int(((time.time()) + 1) + 3600)
-            if not await is_moderator(interaction.user) and not await is_chat_moderator(interaction.user):
+            if not await is_moderator(interaction.user) and not await is_bot_developer(interaction.user) or not await is_chat_moderator(interaction.user):
                   await interaction.send("You do not have the necessary permissions to perform this action", ephemeral = True)
                   return
             user_id = user.id
             user = bot.get_guild(GUILD_ID).get_member(user_id)
+            dm = await user.create_dm()
             view = discord.ui.View(timeout=None)
             proceedBTN = discord.ui.Button(label="Proceed", style=discord.ButtonStyle.blurple)
             cancelBTN = discord.ui.Button(label="Cancel", style=discord.ButtonStyle.red)
+            
             async def proceedCallBack(interaction):
                  await message.delete()
-                 dm = await user.create_dm()
                  embed = discord.Embed(description = f"Study time! You've been given a temporary break from the off-topic channels for the next hour, thanks to <@{interaction.user.id}>. Use this time to focus on your studies and make the most of it!\n\nThe role will be removed at <t:{unmute_tim}:f>, which is <t:{unmute_tim}:R>", color=0xAFE1AF)
                  await dm.send(embed=embed)
                  await user.add_roles(forced_mute_role)
                  await interaction.send(f"{user.name} has been put on forced mute until <t:{unmute_tim}:f>, which is <t:{unmute_tim}:R>.")
                  timern = int(time.time()) + 1
                  unmute_time = int(((time.time()) + 1) + 3600)
-                 client = pymongo.MongoClient(LINK)
-                 db = client.IGCSEBot
-                 mute = db["mute"]
-                 mute.insert_one({"_id": str(timern), "user_id": str(user_id),
-                                  "unmute_time": str(unmute_time), "muted": True})
+                 mute.insert_one({"_id": str(timern), "user_id": str(user_id), "unmute_time": str(unmute_time), "muted": True})
             proceedBTN.callback = proceedCallBack
+            
             async def cancelCallBack(interaction):
                await message.delete()
                cancelBTN.callback = cancelCallBack
@@ -68,14 +68,17 @@ async def gostudy(interaction: discord.Interaction,
             message = await interaction.send("Are you sure?", view=view, ephemeral=True)
 
 @bot.slash_command(name="remove_gostudy", description="remove the Forced Mute role. (for mods)")
-async def remove_gostudy(interaction: discord.Interaction,
-
-                  user: discord.User = discord.SlashOption(name="name", description="who do you want to use this command on?", required=False)):
+async def remove_gostudy(interaction: discord.Interaction, user: discord.User = discord.SlashOption(name="name", description="who do you want to use this command on?", required=False)):
         await interaction.response.defer(ephemeral = True)
-        if not await is_moderator(interaction.user) and not await is_chat_moderator(interaction.user):
+
+        if not await is_moderator(interaction.user) and not await is_bot_developer(interaction.user) or not await is_chat_moderator(interaction.user):
                   await interaction.send("You do not have the necessary permissions to perform this action", ephemeral = True)
                   return
         
+        client = pymongo.MongoClient(LINK)
+        db = client.IGCSEBot
+        mute = db["mute"] 
+
         forced_mute_role = bot.get_guild(GUILD_ID).get_role(FORCED_MUTE_ROLE)
         if user == None:
             user_id = interaction.user.id
@@ -83,9 +86,6 @@ async def remove_gostudy(interaction: discord.Interaction,
             member = guild.get_member(user_id)
             forced_mute_role = bot.get_guild(GUILD_ID).get_role(FORCED_MUTE_ROLE)
             await member.remove_roles(forced_mute_role)
-            client = pymongo.MongoClient(LINK)
-            db = client.IGCSEBot
-            mute = db["mute"]
             mute.delete_one({"user_id": str(user_id)})
             await interaction.send(f"the Forced mute role has been removed from <@{user_id}>.", ephemeral=True)
 
@@ -96,8 +96,5 @@ async def remove_gostudy(interaction: discord.Interaction,
             member = guild.get_member(user_id)
             forced_mute_role = bot.get_guild(GUILD_ID).get_role(FORCED_MUTE_ROLE)
             await member.remove_roles(forced_mute_role)
-            client = pymongo.MongoClient(LINK)
-            db = client.IGCSEBot
-            mute = db["mute"]
             mute.delete_one({"user_id": str(user_id)})
             await interaction.send(f"the Forced mute role has been removed from <@{user_id}>.", ephemeral=True)
