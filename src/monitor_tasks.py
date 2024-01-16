8392a80a009cd476d98ad32b388bb94e4bee11d8
chirag350 <chiraglamba350@gmail.com>
2024-01-16 18:50:00 +0530

    Revert Revert changes

--- src/monitor_tasks.py
+++ src/monitor_tasks.py
@@ -1,6 +1,8 @@
 from constants import LINK, GUILD_ID, FORCED_MUTE_ROLE
 from bot import bot, discord, tasks, pymongo, bot, guild
 import time, traceback
+from data import AUTO_SLOWMODE_CHANNELS
+import datetime
 
 async def togglechannellock(channel_id, unlock, *, unlocktime=0):
     #Function for locking/unlocking a discord channel
@@ -91,3 +93,52 @@ async def checkmute():
 
         except Exception:
             print(traceback.format_exc())
+
+@tasks.loop(seconds=30)
+async def handle_slowmode():
+    for channel_id in AUTO_SLOWMODE_CHANNELS:
+        slowmode = 3
+        current_time = datetime.datetime.now()
+        time_15s_ago = current_time - datetime.timedelta(seconds=15)
+        channel = bot.get_channel(channel_id)
+        if not channel: continue
+        messages_in15s = await channel.history(after=time_15s_ago, limit=300).flatten()
+        number_of_messages = len(messages_in15s)
+        if number_of_messages <= 10:
+            slowmode = 0
+            if channel.slowmode_delay != 0:
+                await channel.edit(slowmode_delay=slowmode)
+            continue
+
+        user_messages = {}
+
+        for message in messages_in15s:
+            if message.author.bot: continue
+            if message.author.id not in user_messages:
+                user_messages[message.author.id] = 1
+            else:
+                user_messages[message.author.id] += 1
+        
+        sorted_user_messages = dict(sorted(user_messages.items(), key=lambda x: x[1], reverse=True))
+        for user_id, message_count in sorted_user_messages.items():
+            if message_count >= 12:
+                message = await channel.send(f"<@{user_id}> You are sending too many messages. Please slow down.")
+                await message.delete(delay=10)
+            
+        if number_of_messages > 60:
+            # 4 messages per second, will likely never happen
+            slowmode = 120
+        elif number_of_messages > 45:
+            # chat is absolute chaos
+            slowmode = 60
+        elif number_of_messages > 30:
+            slowmode = 45
+        elif number_of_messages > 20:
+            slowmode = 15
+        elif number_of_messages > 15:
+            slowmode = 7
+            
+        if channel.slowmode_delay != slowmode:
+            await channel.edit(slowmode_delay=slowmode)
+
+handle_slowmode.start()
