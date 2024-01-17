2758b8d1cc2e4cadd83eb0bd61b8537b2f755408
Juzcallmekaushik <76829835+juzcallmekaushik@users.noreply.github.com>
2024-01-17 13:47:16 +0800

    Update on_ready.py

--- src/on_ready.py
+++ src/on_ready.py
@@ -12,7 +12,7 @@ async def on_ready():
     botlogs = await igcse.fetch_channel(BOTLOG_CHANNEL_ID)
     user = bot.user 
     format = "%d-%m-%Y"           
-    embed = discord.Embed(title=f"r/IGCSE bot restarted successfully!", colour=0x51BB56)
+    embed = discord.Embed(title=f"{bot.user.display_name} restarted successfully!", colour=0x51BB56)
     embed.add_field(name="Bot Information", value=f"```Name: {bot.user}\nCreated on: {'28-02-2022' if BETA else '05-07-2021'}\nJoined on: {'30-09-2022' if BETA else '08-06-2022'}\nBeta: {BETA}\nVerified: {user.verified}\nID: {user.id}```", inline=False)
     embed.add_field(name="Guild Information", value=f"```Name: {igcse.name}\nOwner: {igcse.owner}\nCreated on: {igcse.created_at.strftime(format)}\nMembers: {igcse.member_count}\nBoosts: {igcse.premium_subscription_count}\nID: {igcse.id}```", inline=False)
     embed.add_field(name="Role Statistics", value=f"```IGCSE Helpers: {len(igcse.get_role(IGCSE_HELPER_ROLE).members)}\nAS/AL Helpers: {len(igcse.get_role(AL_HELPER_ROLE).members)}\nBot Developers: {len(igcse.get_role(BOT_DEVELOPER_ROLES).members)}\nStaff Moderators: {len(igcse.get_role(STAFF_MODERATOR_ROLE).members)}\nTemp Moderators: {len(igcse.get_role(TEMP_MOD_ROLE).members)}\nChat Moderators: {len(igcse.get_role(CHAT_MODERATOR_ROLES).members)}```", inline=False)
