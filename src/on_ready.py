7007d51342da94f8a4e9492268c5f4f44b753bc7
juzcallmekaushik <76829835+juzcallmekaushik@users.noreply.github.com>
2024-01-17 09:22:48 +0800

    Update on_ready.py

--- src/on_ready.py
+++ src/on_ready.py
@@ -4,15 +4,16 @@ from monitor_tasks import checklock, checkmute
 
 @bot.event
 async def on_ready():
+    igcse = await bot.fetch_guild(GUILD_ID)
+    botlogs = await igcse.fetch_channel(BOTLOG_CHANNEL_ID)    
     print(f"Logged in as {str(bot.user)}.")
     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="r/IGCSE"))
     checklock.start()
     checkmute.start()    
+    await botlogs.send("embed sending..")
     embed = discord.Embed(title=f"Guilds Info ({len(bot.guilds)})", colour=0x3498db, description="Statistics about the servers this bot is in.")
     for guild in bot.guilds:
         value = f"Owner: {guild.owner}\nMembers: {guild.member_count}\nBoosts: {guild.premium_subscription_count}"
         embed.add_field(name=guild.name, value=value, inline=True)
-    igcse = await bot.fetch_guild(GUILD_ID)
-    botlogs = await igcse.fetch_channel(BOTLOG_CHANNEL_ID)
     await botlogs.send(embed=embed)
     await botlogs.send("embed sent..")
