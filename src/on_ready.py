2de4eef45cc677d681799b08f128da4667b377f0
juzcallmekaushik <76829835+juzcallmekaushik@users.noreply.github.com>
2024-01-17 15:41:08 +0800

    Update on_ready.py

--- src/on_ready.py
+++ src/on_ready.py
@@ -1,6 +1,6 @@
 from bot import bot, discord
 from constants import GUILD_ID, BOTLOG_CHANNEL_ID, BOTBETA, BOTMAIN, BETA, CHAT_MODERATOR_ROLES, IGCSE_HELPER_ROLE, AL_HELPER_ROLE, BOT_DEVELOPER_ROLES, TEMP_MOD_ROLE, STAFF_MODERATOR_ROLE
-from monitor_tasks import checklock, checkmute
+from monitor_tasks import checklock, checkmute, handle_slowmode
 
 @bot.event
 async def on_ready():
@@ -8,13 +8,16 @@ async def on_ready():
     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="r/IGCSE"))
     checklock.start()
     checkmute.start()
+    handle_slowmode.start()
     igcse = bot.get_guild(GUILD_ID)
     botlogs = await igcse.fetch_channel(BOTLOG_CHANNEL_ID)
     user = bot.user 
-    format = "%d-%m-%Y"           
-    embed = discord.Embed(title=f"{bot.user.display_name} restarted successfully!", colour=0x51BB56)
-    embed.add_field(name="Bot Information", value=f"```Name: {bot.user}\nCreated on: {'28-02-2022' if BETA else '05-07-2021'}\nJoined on: {'30-09-2022' if BETA else '08-06-2022'}\nBeta: {BETA}\nVerified: {user.verified}\nID: {user.id}```", inline=False)
-    embed.add_field(name="Guild Information", value=f"```Name: {igcse.name}\nOwner: {igcse.owner}\nCreated on: {igcse.created_at.strftime(format)}\nMembers: {igcse.member_count}\nBoosts: {igcse.premium_subscription_count}\nID: {igcse.id}```", inline=False)
-    embed.add_field(name="Role Statistics", value=f"```IGCSE Helpers: {len(igcse.get_role(IGCSE_HELPER_ROLE).members)}\nAS/AL Helpers: {len(igcse.get_role(AL_HELPER_ROLE).members)}\nBot Developers: {len(igcse.get_role(BOT_DEVELOPER_ROLES).members)}\nStaff Moderators: {len(igcse.get_role(STAFF_MODERATOR_ROLE).members)}\nTemp Moderators: {len(igcse.get_role(TEMP_MOD_ROLE).members)}\nChat Moderators: {len(igcse.get_role(CHAT_MODERATOR_ROLES).members)}```", inline=False)
-    embed.set_footer(text=f"{bot.user}", icon_url=bot.user.display_avatar.url)
-    await botlogs.send(embed=embed)
+    format = "%d-%m-%Y"       
+    if not BETA:    
+        embed = discord.Embed(title=f"{bot.user.display_name} restarted successfully!", colour=0x51BB56)
+        embed.add_field(name="Bot Information", value=f"```Name: {bot.user}\nCreated on: {'28-02-2022' if BETA else '05-07-2021'}\nJoined on: {'30-09-2022' if BETA else '08-06-2022'}\nBeta: {BETA}\nVerified: {user.verified}\nID: {user.id}```", inline=False)
+        embed.add_field(name="Guild Information", value=f"```Name: {igcse.name}\nOwner: {igcse.owner}\nCreated on: {igcse.created_at.strftime(format)}\nMembers: {igcse.member_count}\nBoosts: {igcse.premium_subscription_count}\nID: {igcse.id}```", inline=False)
+        embed.add_field(name="Role Statistics", value=f"```No. of roles: {len(igcse.roles)}\nIGCSE Helpers: {len(igcse.get_role(IGCSE_HELPER_ROLE).members)}\nAS/AL Helpers: {len(igcse.get_role(AL_HELPER_ROLE).members)}\nBot Developers: {len(igcse.get_role(BOT_DEVELOPER_ROLES).members)}\nStaff Moderators: {len(igcse.get_role(STAFF_MODERATOR_ROLE).members)}\nTemp Moderators: {len(igcse.get_role(TEMP_MOD_ROLE).members)}\nChat Moderators: {len(igcse.get_role(CHAT_MODERATOR_ROLES).members)}```", inline=False)
+        embed.add_field(name="Channels & Commands", value=f"```No. of users: {len(igcse.humans)}\nNo. of bots: {len(igcse.bots)}\nNo. of catagories: {len(igcse.categories)}\nNo. of text-channels: {len(igcse.text_channels)}\nNo. of voice-channels: {len(igcse.voice_channels)}\nNo. of forum-channels: {len(igcse.forum_channels)}\nNo. of slash-commands: {len(bot.get_all_application_commands())}```", inline=False)
+        embed.set_footer(text=f"{bot.user}", icon_url=bot.user.display_avatar.url)
+        await botlogs.send(embed=embed)
