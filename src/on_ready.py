fbaf227d7b61b0076f67e119dac853009e227975
Juzcallmekaushik <76829835+juzcallmekaushik@users.noreply.github.com>
2024-01-17 12:15:22 +0800

    revert back to the original on_ready.

--- src/on_ready.py
+++ src/on_ready.py
@@ -10,11 +10,8 @@ async def on_ready():
     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="r/IGCSE"))
     checklock.start()
     checkmute.start()    
-    try:
-        embed = discord.Embed(title=f"Guilds Info ({len(bot.guilds)})", colour=0x3498db, description="Statistics about the servers this bot is in.")
-        for guild in bot.guilds:
-            value = f"Owner: {guild.owner}\nMembers: {guild.member_count}\nBoosts: {guild.premium_subscription_count}"
-            embed.add_field(name=guild.name, value=value, inline=True)
-        await botlogs.send(embed=embed)
-    except Exception:
-        await botlogs.send(f"```py{Exception}````")
+    embed = discord.Embed(title=f"Guilds Info ({len(bot.guilds)})", colour=0x3498db, description="Statistics about the servers this bot is in.")
+    for guild in bot.guilds:
+        value = f"Owner: {guild.owner}\nMembers: {guild.member_count}\nBoosts: {guild.premium_subscription_count}"
+        embed.add_field(name=guild.name, value=value, inline=True)
+    await botlogs.send(embed=embed)
