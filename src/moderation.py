66d0dc43e65d11c69db1d5979d3f50c1f38703b8
Juzcallmekaushik <76829835+juzcallmekaushik@users.noreply.github.com>
2024-01-19 14:49:54 +0800

    Update moderation.py

--- src/moderation.py
+++ src/moderation.py
@@ -90,9 +90,10 @@ async def history(interaction: discord.Interaction, user: discord.User = discord
         await interaction.send(f"{user} does not have any previous offenses.", ephemeral=False)
     else:
         text = f"Moderation History for {user}:\n\n"
+        text = f"No. of offences:\n"
         text += "\n".join(list(map(lambda x:f"{x[0]}: {x[1]}", list(actions.items()))))
-        text += f"\nTotal number of offences: {total}"
-        text += '\n\n'
+        text += f"\nTotal no. of offences: {total}\n\n"
+        text += f"\nFurther Details:\n"
         text += ('\n'.join(history))[:1900]
         await interaction.send(f"```{text}```", ephemeral=False)
 
