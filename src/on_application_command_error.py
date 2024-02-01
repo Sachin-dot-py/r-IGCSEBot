55cf451d0f1d5ca117988b5dc8dfb92311b3f650
Juzcallmekaushik <76829835+juzcallmekaushik@users.noreply.github.com>
2024-02-01 16:47:41 +0800

    Update on_application_command_error.py

--- src/on_application_command_error.py
+++ src/on_application_command_error.py
@@ -1,15 +1,9 @@
 from bot import bot, traceback
-from discord import Embed
 from constants import GUILD_ID, BOTLOG_CHANNEL_ID
 
 @bot.event
 async def on_application_command_error(interaction, exception):
-    embed = Embed()
-
-    embed.title = "An exception has occurred!"
-
-    embed.add_field(value=f"channel: {interaction.channel.mention}\nUser: {interaction.user.mention}\nGuild: {interaction.guild.name} ({interaction.guild.id})", inline=False)
-    embed.add_field(name="Error", value=f"```{''.join(traceback.format_exception(exception, exception, exception.__traceback__))}```", inline=False)
-
+    description = f"Channel: {ctx.channel.mention}\nUser: {ctx.author.mention}\nGuild: {ctx.guild.name} ({ctx.guild.id})\n\nError:\n```{''.join(traceback.format_exception(exception, exception, exception.__traceback__))}```}    
+    embed = discord.Embed(title="An Exception Occured", description=description)
     botlogs = bot.get_channel(BOTLOG_CHANNEL_ID)
     await botlogs.send(embed=embed)
