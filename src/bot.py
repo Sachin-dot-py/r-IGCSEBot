d58389bfa08a5aa213a99edac0c7b22e34ae0796
Chirag L <chiraglamba350@gmail.com>
2024-02-02 05:02:50 +0530

    disallow @everyone mentions using the bot globally (#137)

--- src/bot.py
+++ src/bot.py
@@ -16,6 +16,6 @@ from nextcord.ext import tasks, commands
 from constants import GUILD_ID
 
 intents = discord.Intents().all()
-bot = commands.Bot(command_prefix=".", intents=intents)
+bot = commands.Bot(command_prefix=".", intents=intents, allowed_mentions=discord.AllowedMentions(everyone=False))
 guild = bot.get_guild(GUILD_ID)
 keywords = {}
