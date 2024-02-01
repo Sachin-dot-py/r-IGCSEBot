738dae0170c24f29e99c167e76348063decde79d
Juzcallmekaushik <76829835+juzcallmekaushik@users.noreply.github.com>
2024-02-01 16:41:38 +0800

    Update on_application_command_error.py

--- src/on_application_command_error.py
+++ src/on_application_command_error.py
@@ -8,9 +8,7 @@ async def on_application_command_error(interaction, exception):
 
     embed.title = "An exception has occurred!"
 
-    embed.add_field(name="Channel", value=interaction.channel.mention, inline=False)
-    embed.add_field(name="User", value=interaction.user.mention, inline=False)
-    embed.add_field(name="Guild", value=f"{interaction.guild.name} ({interaction.guild.id})", inline=False)
+    embed.add_field(value=f"channel: {interaction.channel.mention}\nUser: {interaction.user.mention}\nGuild: {interaction.guild.name} ({interaction.guild.id})", inline=False)
     embed.add_field(name="Error", value=f"```{''.join(traceback.format_exception(exception, exception, exception.__traceback__))}```", inline=False)
 
     botlogs = bot.get_channel(BOTLOG_CHANNEL_ID)
